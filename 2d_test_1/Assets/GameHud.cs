eb647f1ad30ebe321ed72635249533936e377139
Danish <dan.my1313@gmail.com>
2018-07-12 23:11:22 +0530

    Added .gitignore file, first push

---
+++ 2d_test_1/Assets/GameHud.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHud : MonoBehaviour {
+    public GameState gameState;
+    public GameObject spinWheelBtn;
+    public Text number;
+    public Text betNumber;
+    public int betNo;
+    public GameObject mainSPinButton;
+    public GameObject mainImage;
+
+    private void Awake()
+    {
+        gameState = GameState.DEFAULT;
+        mainSPinButton.SetActive(false);
+        if (spinWheelBtn.activeSelf)
+        {
+            spinWheelBtn.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        this.betNumber.text = "PLEASE SELECT BET NUMBER";
+    }
+
+    public void SetText(int luckyNumber, int betNO) {
+        if(luckyNumber == betNo)
+        {
+            number.text = " <color=green>YOU WIN</color>   Number is :: " + luckyNumber.ToString();
+
+        }
+        else
+        {
+            number.text = " <color=red>YOU LOST</color>   Number is :: " + luckyNumber.ToString();
+
+        }
+    }
+
+    public void spinWheel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ActivateSpinBtn(GameObject go)
+    {
+        if (!go.activeSelf)
+        {
+            go.SetActive(true);
+        }
+
+    }
+
+    public void DeactivateSpinBtn(GameObject go)
+    {
+        if (!go.activeSelf)
+        {
+            go.SetActive(true);
+        }
+
+    }
+
+    public void SetBetNumber(int betNumber)
+    {
+        this.betNo = betNumber;
+        this.betNumber.text = "BET NUMBER : " + this.betNo.ToString();
+        mainSPinButton.SetActive(true);
+
+    }
+
+    public void HideImage()
+    {
+        mainImage.SetActive(false);
+        gameState = GameState.RUNNING;
+        mainSPinButton.SetActive(false);
+    }
+
+}
+[System.Serializable]
+public enum GameState
+{
+    RUNNING,
+    NOT_RUNNNG,
+    DEFAULT
+}
---
+++ 2d_test_1/Assets/ValuePoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValuePoint : MonoBehaviour {
+    public int serialNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
