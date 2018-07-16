91a222af9af0e7f51de90c6847d82b73b1a96b73
amir javed <javedamir135@gmail.com>
2018-07-17 01:50:48 +0530

    splash, menu scene added

---
+++ Roulette_2d/Assets/SplashScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadNextScene ());
+	}
+	
+	IEnumerator loadNextScene(){
+		yield return new WaitForSeconds (0.5f);
+		//put initialization hold here
+
+		SceneManager.LoadScene ("GameScene");
+	}
+
+}
